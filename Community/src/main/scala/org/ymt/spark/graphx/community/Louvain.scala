39adb2cd026ae76603dbb296de027d078b8c9c79
yangmutong <985777876@qq.com>
2017-05-10 20:18:52 +0800

    remove

--- Community/src/main/scala/org/ymt/spark/graphx/community/Louvain.scala
+++ Community/src/main/scala/org/ymt/spark/graphx/community/Louvain.scala
@@ -61,7 +61,7 @@ object Louvain extends Serializable{
   }
 
   def makeGraph[VD: ClassTag](inputPath: String, sc: SparkContext, numPartitions: Int): Graph[Double, Double] = {
-    GraphLoader.edgeListFile(sc, inputPath, canonicalOrientation=true, numEdgePartitions=numPartitions).unpersist()
+    GraphLoader.edgeListFile(sc, inputPath, numEdgePartitions=numPartitions).unpersist()
       .partitionBy(PartitionStrategy.EdgePartition2D).unpersist()
       .mapVertices((vid, attr) => attr.toDouble).unpersist()
       .mapEdges(v => v.attr.toDouble)
